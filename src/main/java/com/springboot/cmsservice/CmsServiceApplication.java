396a24db9c8471ff3a4aaabbcbdbda842122e644
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-10-07 22:45:42 +0300

    Tomcat Deployment

--- src/main/java/com/springboot/cmsservice/CmsServiceApplication.java
+++ src/main/java/com/springboot/cmsservice/CmsServiceApplication.java
@@ -2,9 +2,16 @@ package com.springboot.cmsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CmsServiceApplication {
+public class CmsServiceApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CmsServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsServiceApplication.class, args);
